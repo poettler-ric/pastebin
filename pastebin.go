f0269b2152e6e8d5b826289c46278f1c7b60fa72
Richard Poettler <richard.poettler@gmail.com>
2019-05-28 17:47:29 +0200

    Rewrite with authentification and go apis

--- pastebin.go
+++ pastebin.go
@@ -1,90 +1,72 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"path/filepath"
 )
-
-const (
-	// URL to the github api
-	githubAPIURL = "https://api.github.com"
-	// content type for the json request
-	jsonContentType = "application/json"
-	// location for the gist api on api.github.com
-	gistLocation = "/gists"
-)
-
-// CreateData holds the information to create a gist
-type CreateData struct {
-	Description string              `json:"description"`
-	Public      bool                `json:"public"`
-	Files       map[string]FileData `json:"files"`
-}
-
-// FileData holds the information about file entries in CreateData
-type FileData struct {
-	Content string `json:"content"`
-}
-
-// CreateResponse holds response information after creating a gist
-type CreateResponse struct {
-	ID      string `json:"id"`
-	HTMLURL string `json:"html_url"`
-}
 
 func main() {
-	var data CreateData
-	data.Public = true
-	data.Files = make(map[string]FileData)
+	// read config
+	viper.SetConfigName("pastebin")
+	viper.AddConfigPath("$HOME/.pastebin")
+	viper.ReadInConfig()
 
-	if len(os.Args) <= 1 {
-		// post stdin
-		content, err := ioutil.ReadAll(os.Stdin)
-		if err != nil {
-			log.Fatalf("error while reading from stdin: %s", err)
-		}
-		data.Files["stdin"] = FileData{string(content)}
-	} else {
-		// post given files
-		for _, a := range os.Args[1:] {
-			content, err := ioutil.ReadFile(a)
+	pflag.StringP("token", "t", "", "github api token to use")
+	pflag.BoolP("public", "p", false, "create public gist")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	// create client
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: viper.GetString("token")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// create gist
+	gist := new(github.Gist)
+	gist.Files = make(map[github.GistFilename]github.GistFile)
+	public := viper.GetBool("public")
+	gist.Public = &public
+
+	// fill gist
+	if files := pflag.Args(); len(files) > 0 {
+		// post files
+		for _, f := range files {
+			bytes, err := ioutil.ReadFile(f)
 			if err != nil {
-				log.Fatalf("error while reading %s: %s", a, err)
+				log.Fatalf("error while reading %s: %v", f, err)
 			}
-			data.Files[filepath.Base(a)] = FileData{string(content)}
+			content := string(bytes)
+			gist.Files[github.GistFilename(f)] = github.GistFile{
+				Content: &content,
+			}
+		}
+	} else {
+		// post stdin
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("error while reading stdin: %v", err)
+		}
+
+		content := string(bytes)
+		gist.Files["stdin"] = github.GistFile{
+			Content: &content,
 		}
 	}
 
 	// post gist
-	out, err := json.Marshal(data)
+	gist, _, err := client.Gists.Create(ctx, gist)
 	if err != nil {
-		log.Fatalf("error while marshalling: %s", err)
+		log.Fatalf("error while creating gist: %v", err)
 	}
-
-	res, err := http.Post(githubAPIURL+gistLocation, jsonContentType, bytes.NewReader(out))
-	if err != nil {
-		log.Fatalf("error while posting: %s", err)
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	if res.StatusCode == 201 {
-		// print gist url
-		var creationInfo CreateResponse
-		err = json.Unmarshal(body, &creationInfo)
-		if err != nil {
-			log.Fatalf("error while parsing json: %s", err)
-		}
-		fmt.Printf("created gist: %s\n", creationInfo.HTMLURL)
-	} else {
-		// print error message
-		fmt.Printf("failed to create gist:\n%s\n%s\n", res.Status, body)
-	}
+	fmt.Println(*gist.HTMLURL)
 }
